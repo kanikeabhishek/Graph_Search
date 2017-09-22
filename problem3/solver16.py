e1a4627cc0659f83d4ecc4574fce0fd811cdf3ec
“Chuhua <whale9067@hotmail.com>
2017-09-22 00:22:39 -0400

    v2

--- problem3/solver16.py
+++ problem3/solver16.py
@@ -4,45 +4,96 @@ import random
 import Queue as Q
 
 
-goal_state = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
-puzzle_num = random.sample(range(16), 16)
-initial_puzzle = [puzzle_num[i:i + 4] for i in xrange(0, len(puzzle_num), 4)]
-initial_puzzle = [[1,2,4,3],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+class puzzle_15:
+
+    def __init__(self):
+        self.zero = [0,0]
+        self.initial_puzzle = [[1, 2, 4, 3], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
+        self.puzzle_num = random.sample(range(16), 16)
+        self.initial_puzzle = [self.puzzle_num[i:i + 4] for i in xrange(0, len(self.puzzle_num), 4)]
+        self.goal_state = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
+
+    def heuristic(self,puzzle):
+        count = 0
+        for i in range(4):
+            for j in range(4):
+                if puzzle[i][j] != self.goal_state[i][j]:
+                    count += 1
+        return (count/3)
+
+    def is_goal(self,puzzle):
+        return puzzle == self.goal_state
+
+    def find_empty(self,puzzle):
+        for row in range(4):
+            for col in range(4):
+                if puzzle[row][col] == 0:
+                    return row, col
+
+    def move(self, puzzle, direction, size):
+        puzzle_temp = puzzle
+        if direction == 'L':
+            if size == 1:
+                puzzle_temp[self.zero[0]][self.zero[1]] = puzzle_temp[self.zero[0]][self.zero[1]+1]
+                puzzle_temp[self.zero[0]][self.zero[1]+1] = 0
+                self.zero = [self.zero[0],self.zero[1]+1]
+            elif size == 2:
+                puzzle_temp[self.zero[0]][self.zero[1]] = puzzle_temp[self.zero[0]][self.zero[1]+1]
+                puzzle_temp[self.zero[0]][self.zero[1]+1] = puzzle_temp[self.zero[0]][self.zero[1]+2]
+                puzzle_temp[self.zero[0]][self.zero[1]+2] = 0
+                self.zero = [self.zero[0], self.zero[1] + 2]
+
+            elif size == 3:
+                puzzle_temp[self.zero[0]][self.zero[1]] = puzzle_temp[self.zero[0]][self.zero[1]+1]
+                puzzle_temp[self.zero[0]][self.zero[1]+1] = puzzle_temp[self.zero[0]][self.zero[1]+2]
+                puzzle_temp[self.zero[0]][self.zero[1]+2] = puzzle_temp[self.zero[0]][self.zero[1]+3]
+                puzzle_temp[self.zero[0]][self.zero[1]+3] = 0
+                self.zero = [self.zero[0], self.zero[1] + 3]
 
 
-def heuristic(puzzle):
-    count = 0
-    for i in range(4):
-        for j in range(4):
-            if puzzle[i][j] != goal_state[i][j]:
-                count += 1
-    return (count/3)
+        elif direction == 'R':
+            if size == 1:
+                puzzle_temp[zero[0]][0] = puzzle_temp[zero[0]][1]
+                puzzle_temp[zero[0]][1] = 0
+            elif size == 2:
+                puzzle_temp[zero[0]][0] = puzzle_temp[zero[0]][1]
+                puzzle_temp[zero[0]][1] = puzzle_temp[zero[0]][2]
+                puzzle_temp[zero[0]][2] = 0
+            elif size == 3:
+                puzzle_temp[zero[0]][0] = puzzle_temp[zero[0]][1]
+                puzzle_temp[zero[0]][1] = puzzle_temp[zero[0]][2]
+                puzzle_temp[zero[0]][2] = puzzle_temp[zero[0]][3]
+                puzzle_temp[zero[0]][3] = 0
+        elif direction == 'U':
+            temp = tempArr[0][i]
+            tempArr[0][i] = tempArr[1][i]
+            tempArr[1][i] = tempArr[2][i]
+            tempArr[2][i] = tempArr[3][i]
+            tempArr[3][i] = temp
+        elif direction == 'D':
+            temp = tempArr[3][i]
+            tempArr[3][i] = tempArr[2][i]
+            tempArr[2][i] = tempArr[1][i]
+            tempArr[1][i] = tempArr[0][i]
+            tempArr[0][i] = temp
+        return tempArr
 
 
-# check if puzzle is a goal state
-def is_goal(puzzle):
-    return puzzle == goal_state
+    def read_puzzle(file_directory):
+        puzzle = []
+        with open(file_directory, 'rb') as f:
+            for item in f:
+                element = item.split(" ")
+                row = []
+                for num in element:
+                    row.append(int(num))
+                puzzle.append(row)
+        return puzzle
 
-def move_vertical():
-
-def move_horizontal():
-
-
-def read_puzzle(file_directory):
-    puzzle = []
-    with open(file_directory, 'rb') as f:
-        for item in f:
-            element = item.split(" ")
-            row = [] 
-            for num in element:
-                row.append(int(num))
-            puzzle.append(row)
-    return puzzle
-
-def print_puzzle(puzzle):
-    for i in puzzle:
-        print i
-    print "\n"
+    def print_puzzle(puzzle):
+        for i in puzzle:
+            print i
+        print "\n"
 
 
 
